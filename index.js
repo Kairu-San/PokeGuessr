e1a62f41e75d4e5d454b14e732945f9a7722e810
Kairu-San <kylelegaspi10@gmail.com>
2025-09-01 01:00:50 +0800

    Make next button function

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 //Call for pokefetch everytime site loads
 window.addEventListener("load", () => {
-    pokefetch();
+    pokefetchreload();
 });
 
-async function pokefetch() {
+async function pokefetchreload() {
 
     //RNG setter everytime site loads
     const randomNumber = Math.floor(Math.random() * 1025) + 1;
-
-    // Calls pokemon from input
     const rngReload = randomNumber;
-    const guessInput = document.getElementById("guess").value.toLowerCase();
-
+    
+    //Next button reloads website
+    const next = document.getElementById("next");
+    next.addEventListener("click", () => {location.reload();});
+    
     const pokemonName = rngReload;
     const responce = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
-
-
-    if (!responce.ok) {
-        throw new Error("Could not fetch resource");
-    }
-
+    //Turns responce into json file
     const data = await responce.json();
     console.log(data);
+    
     const pokeSprite = data.sprites.other["official-artwork"].front_default;
     const pokeImg = document.getElementById("pokesprite");
 
     pokeImg.src = pokeSprite;
+    pokeImg.style.display = "block";
     
-
 }
+
+function pokefetch() {
+    const guessInput = document.getElementById("guess").value.toLowerCase();
+}
+
